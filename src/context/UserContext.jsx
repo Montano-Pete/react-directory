e73b5815be041775955bc0d3da235339d34c7a15
Peter <pete225848@gmail.com>
2022-01-07 08:32:06 -0800

    created UserContext and useUser hook

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from 'react';
+import { getProfile } from '../services/users';
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const currentUser = getProfile();
+  const [user, setUser] = useState(
+    currentUser ? { id: currentUser.id, email: currentUser.email } : {}
+  );
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export { UserContext, UserProvider };
