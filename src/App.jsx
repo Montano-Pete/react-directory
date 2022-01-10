35fc5020e393751ce73cf28b33f4fe1fe7ffb98a
Peter <pete225848@gmail.com>
2022-01-09 23:54:23 -0800

    added routes to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <UserProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <PrivateRoute exact path="/profile">
+            <Profile />
+          </PrivateRoute>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </UserProvider>
+  );
 }
