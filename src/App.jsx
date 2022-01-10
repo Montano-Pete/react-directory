4dc8f52fbb2cc70bb614ff5b799a9685e8cb8572
Peter <pete225848@gmail.com>
2022-01-09 23:56:29 -0800

    imported components to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { UserProvider } from './context/UserContext';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Home from './components/Home';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Profile from './components/Profile';
 
 export default function App() {
   return (
